fc29ef2ecc30b6ffd8ed52573d5ff71550e4fa42
Adamf124 <adam.ferguson124@gmail.com>
2018-06-27 03:48:31 -0400

    create clean intro words

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React on 3000</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="Intro-words">
+          <h1>
+            <div>Where</div> <div>ideas</div> <div>come</div> <div>to</div> <div>reach</div> <div>their</div> <div>platform.</div>
+          </h1>
+        </div>
       </div>
     );
   }
