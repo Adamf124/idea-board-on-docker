958644ff5107797155dfc5048b9ca728e1fb420b
Adamf124 <adam.ferguson124@gmail.com>
2018-06-27 15:53:32 -0400

    added main title, animations and styling

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -5,23 +5,26 @@ import drawingboard from '../StockVideos/mandrawingboard.mp4';
 class Dashboard extends Component {
   render() {
     return (
+
       <div className="App">
     {/* // content container */}
-    <div className="main-container">
-      <div className="Intro-words">
-        <h1>
-          <div>Where</div> <div>ideas</div> <div>come</div> <div>to</div> <div>reach</div> <div>their</div> <div>platform.</div>
-        </h1>
-      </div>
+        <div className="main-container">
+          <div className="Main-title">Reachspace: An Idea Launchpad</div>
+            <div className="Intro-words">
+              <h1>
+                <div>Where</div> <div>ideas</div> <div>come</div> <div>to</div> <div>reach</div> <div>their</div> <div>platform.</div>
+              </h1>
+            </div>
 
         {/* parallax video */}
         <div className="parallax-container">
           <video id="background-video" loop autoPlay>
             <source src={drawingboard} type="video/mp4" />
           </video>
-          </div>
-          <div className="Description-Container"><h1>We are in the business of spreading ideas.</h1>
-          </div>
+        </div>
+        <div className="Description-Container"><h1>We are in the business of spreading ideas.</h1>
+          
+        </div>
     </div>    
         </div>
 
