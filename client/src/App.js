1afe6fd0cfbee1ba0b40a1bdb05f5477456678e4
Adamf124 <adam.ferguson124@gmail.com>
2018-06-28 21:09:30 -0400

    added login route and switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { Link, Switch, BrowserRouter as Router, Route } from 'react-router-dom'
 import Dashboard from './components/Dashboard'
+import LogInPage from './components/LogInPage'
 
 class App extends Component {
   render() {
     return (
       <Router>
+        <Switch>
         <Route exact path="/" component={Dashboard} />
         <Route path="/login" component={LogInPage}/>
+        </Switch>
       </Router>
         );
       }
