287ee279c4a6f2a013211cade26e70f524aa017b
Adamf124 <adam.ferguson124@gmail.com>
2018-06-29 15:14:44 -0400

    create Login Page

---
+++ client/src/components/LogInPage.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
+
+class LogInPage extends Component {
+    state = {
+      users: []
+    }
+    componentDidMount (){
+         axios.get('/api/users')
+         .then(res => {
+            console.log(res.data)
+          this.setState({users: res.data.users})
+        })
+    }
+    // listAllUsers = () => {
+    //     axios.get(`localhost:3001/api/users`)
+    //     .then(res => {
+    //       this.setState({users: res.data})
+    //     })
+    //   }
+
+      render () {
+        return (
+          <div>
+            <h1>Log-In</h1>
+            <h3>Please Select YOURSELF</h3>
+            {this.state.users.map(user => {
+              return (<Link to={`/user/${user._id}`}>{user.username}</Link>)
+            })}
+          </div>
+        )
+      }
+    }
+    
+    export default LogInPage
