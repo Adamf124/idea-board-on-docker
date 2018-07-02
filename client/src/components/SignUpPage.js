561a1d96476052ea051f33405b9e4e75497c211e
Adamf124 <adam.ferguson124@gmail.com>
2018-07-02 04:59:55 -0400

    Styling

--- client/src/components/SignUpPage.js
+++ client/src/components/SignUpPage.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import 'bulma/css/bulma.css'
 import axios from 'axios'
+import '../css/SignupPage.css'
 // eslint-disable-next-line
 import { Link, Switch, BrowserRouter as Router, Route } from 'react-router-dom'
+
 
 class SignupPage extends Component {
   state = {
@@ -31,44 +33,75 @@ class SignupPage extends Component {
 
   render() {
     return (
-
-<div>
-  <h3>Create a User</h3>
+      <div class="page-container">
+  <div className="form-container">
+  <h1>Create a User</h1>
   <form onSubmit={this.handleSubmit}>
-    <input
+  
+  <div className="field">
+  <h3 class="label">Name</h3>
+    <input 
+    class="input"
     placeholder="Full Name"
     name="name"
     type="text"
     value={this.state.name}
     onChange={this.handleChange}
     />
+  </div>
 
-    <input
-      placeholder="User Name"
+  <div className="field">
+  <h3 class="label">Username</h3>
+    <input 
+      class="input"
+      placeholder="Enter Username"
       type="text"
       name="username"
       value={this.state.username}
       onChange={this.handleChange}
     />
+    </div>
 
-    <input
-      placeholder="Password"
+    <div className="field">
+    <h3 class="label">Password</h3>
+    <input 
+      class="input"
+      placeholder="Enter Password"
       type="password"
       name="password"
       value={this.state.password}
       onChange={this.handleChange}
     />
-    <input
-      placeholder="Email Address"
+    </div>
+
+    <div className="field">
+    <h3 class="label">Email Address</h3>
+    <input 
+      class="input" 
+      placeholder="Enter Valid Email Address"
       type="email"
       name="email_address"
       value={this.state.email_address}
       onChange={this.handleChange}
     />
+    </div>
 
-    <button type="submit">Submit</button>
+    <div class="field is-grouped">
+  <p class="control">
+    <button type='submit' class="button is-primary">
+      Submit
+    </button>
+  </p>
+  <p class="control">
+    <Link to="/">
+    <a class="button is-light">
+      Cancel</a>
+    </Link>
+  </p>
+</div>
   </form>
       </div>
+  </div>
     )
   }
 }
