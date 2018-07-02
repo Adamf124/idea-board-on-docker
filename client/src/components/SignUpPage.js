51a06da4b956dbe3ff08229364467cb18a57496b
Adamf124 <adam.ferguson124@gmail.com>
2018-07-01 20:54:17 -0400

    Create Posting signup page

--- client/src/components/SignUpPage.js
+++ client/src/components/SignUpPage.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import 'bulma/css/bulma.css'
+import axios from 'axios'
 // eslint-disable-next-line
 import { Link, Switch, BrowserRouter as Router, Route } from 'react-router-dom'
 
 class SignupPage extends Component {
+  state = {
+    name: '',
+    userName: '',
+    password: '',
+    email_address: ''
 
+  }
 
-  
+  handleChange = (event) => {
+    const inputName = event.target.name
+    const userInput = event.target.value
+
+    this.setState({
+      [ inputName ]: userInput
+    })
+  }
+  handleSubmit = (event) => {
+    event.preventDefault()
+    axios.post('/api/users', this.state).then((res) => {
+      console.log(res.data)
+      this.props.history.push(`/users/${res.data._id}`)
+    })
+}
+
   render() {
     return (
 
@@ -14,17 +36,33 @@ class SignupPage extends Component {
   <h3>Create a User</h3>
   <form onSubmit={this.handleSubmit}>
     <input
+    placeholder="Full Name"
+    name="name"
+    type="text"
+    value={this.state.name}
+    onChange={this.handleChange}
+    />
+
+    <input
       placeholder="User Name"
       type="text"
-      name="userName"
-      value={this.state.userName}
+      name="username"
+      value={this.state.username}
       onChange={this.handleChange}
     />
+
     <input
       placeholder="Password"
       type="password"
       name="password"
       value={this.state.password}
+      onChange={this.handleChange}
+    />
+    <input
+      placeholder="Email Address"
+      type="email"
+      name="email_address"
+      value={this.state.email_address}
       onChange={this.handleChange}
     />
 
