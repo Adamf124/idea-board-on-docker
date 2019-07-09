bd88d730b3a08236d587b7e80d75078be958fdee
Adam <adam.ferguson124@gmail.com>
2019-07-09 17:13:59 -0400

    seperated server and client

--- server/app.js
+++ server/app.js
@@ -9,28 +9,37 @@ const app = express();
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
-mongoose.connect(process.env.MONGODB_URI); 
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true
+});
 
 const connection = mongoose.connection;
 connection.on('connected', () => {
     console.log('===============================')
-    console.log('MONGOOSE CONNECTION ESTABLISHED'); 
-    console.log('===============================')   
-}); 
+    console.log('MONGOOSE CONNECTION ESTABLISHED');
+    console.log('===============================')
+});
 connection.on('error', (err) => {
     console.log('!!!!!!!! ALERT !!!!!!!!!!')
     console.log('Mongoose default connection error: ' + err);
-}); 
+});
 
+port = process.env.PORT || 4000;
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(__dirname + '/client/build/'))
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
+app.listen(port);
 
-app.get('/', (req,res) => {
+console.log('Server is running on port ' + port)
+console.log("You're all set Space Cowboy")
+
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/client/build/index.html')
-  })
+})
 module.exports = app;
